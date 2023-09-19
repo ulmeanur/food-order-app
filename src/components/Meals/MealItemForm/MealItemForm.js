4ff1e6baa4a9882618d9d7ff3e0e59e4554695d5
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-09-20 00:13:41 +0300

    add cart context, cart reducer to show how many item were added in the cart

--- src/components/Meals/MealItemForm/MealItemForm.js
+++ src/components/Meals/MealItemForm/MealItemForm.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../UI/Input/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+	const [amountIsValid, setAmountIsValid] = useState(true);
+	const amountInputRef = useRef();
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredAmount = amountInputRef.current.value;
+		//as the ref value will always be a string we need to converted to number
+		const enteredAmountNumber = +enteredAmount;
+
+		if (
+			enteredAmount.trim().length === 0 ||
+			enteredAmountNumber < -1 ||
+			enteredAmountNumber > 10
+		) {
+			console.log('No items added');
+			setAmountIsValid(false);
+			return;
+		}
+		console.log("enteredAmountNumber=", enteredAmountNumber);
+		props.onAddToCart(enteredAmountNumber);
+	};
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<Input
+				ref={amountInputRef}
 				label="Amount"
 				input={{
 					id: 'amount_' + props.id,
@@ -16,7 +39,8 @@ const MealItemForm = (props) => {
 					defaultValue: '1',
 				}}
 			/>
-			<button>+ Add</button>
+			<button type="submit">+ Add</button>
+			{!amountIsValid && <p>Error! Please enter a valid amount (1-5).</p>}
 		</form>
 	);
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
 import CartContext from './cart-context';
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+//the Cart State
+const cartReducer = (state, action) => {
+	// state is a state object and the last state snapshop managed by the Reducer
+	// action is the action (a number or a text or an object) the will triggered by the Reducer's dispatche function
+ 
+	// cartReducer func is created outside the component
+	// as it doesn't need anything from the component
+	// and it shoudn't need to be recreated everytime when the component is re-evaluated
+
+    if( action.type === "ADD") {
+		console.log(" ADD item", action.item);
+        // concat() generates a brand new array instead of push() that adds a new item
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+
+		//return a new state snapshop
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+    if( action.type === "REMOVE") {
+        
+    }
+
+	return defaultCartState;
+};
 
 const CartProvider = (props) => {
+	// useReducer is used to manage Cart state as it is more complex
 
-    const addItemToCartHandler = (item) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
+	const addItemToCartHandler = (item) => {
+		console.log("dispatchCartAction --> ADD");
+        dispatchCartAction({type: "ADD", item: item});
     };
 
-    const removeItemToCartHandler = (id) => {
-        
+	const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: "REMOVE", id: id});
     };
 
-
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemToCartHandler
-    };
+	const cartContext = {
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
+		addItem: addItemToCartHandler,
+		removeItem: removeItemFromCartHandler,
+	};
 
 	return (
 		<CartContext.Provider value={cartContext}>
-            {props.children}
+			{props.children}
 		</CartContext.Provider>
 	);
 };
