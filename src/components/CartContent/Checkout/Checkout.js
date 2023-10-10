56263e804b11f9c291f8f530d0fea8d4b4c9b503
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-10-11 00:44:57 +0300

    added checkout details form on cart

--- src/components/CartContent/Checkout/Checkout.js
+++ src/components/CartContent/Checkout/Checkout.js
@@ -1,40 +1,43 @@
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
-    const submitHandler = (event) => {
-        event.preventdefault();
+	const confirmHandler = (event) => {
+		event.preventdefault();
+	};
 
-    };
+	const checkoutHandler = (event) => {
+		event.preventDefault();
+	};
 
-    const checkoutHandler = (event) => {
-        event.preventDefault();
-
-    };
-
-
-	return <div className={classes.checkout}>
-        <form onSubmit={submitHandler}>
-            <div className={classes.control}>
-                <label htmlFor=''>Name</label>
-                <input id='name' type='text'/>
-            </div>
-            <div className={classes.control}>
-                <label htmlFor=''>Street</label>
-                <input id='street' type='text'/>
-            </div>
-            <div className={classes.control}>
-                <label htmlFor=''>Postal Code</label>
-                <input id='postal' type='text'/>
-            </div>
-            <div className={classes.control}>
-                <label htmlFor=''>City</label>
-                <input id='postal' type='text'/>
-            </div>
-
-            <button onClick={checkoutHandler}>Confirm</button>
-            <button type='button' onClick={props.onCancel}>Cancel</button>
-        </form>
-    </div>;
+	return (
+		<div className={classes.checkout}>
+			<h2>Checkout details</h2>
+			<form onSubmit={confirmHandler}>
+				<div className={classes.control}>
+					<label htmlFor="">Name</label>
+					<input id="name" type="text" />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="">Street</label>
+					<input id="street" type="text" />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="">Postal Code</label>
+					<input id="postal" type="text" />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="">City</label>
+					<input id="postal" type="text" />
+				</div>
+				<div className={classes.actions}>
+					<button className={classes.submit} onClick={checkoutHandler}>Confirm</button>
+					<button type="button" onClick={props.onCancel}>
+						Cancel
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Checkout;
